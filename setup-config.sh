d6799848b0b7531639191886744df83419b66515
Ayaka <KiritaniAyaka@users.noreply.github.com>
2024-10-08 10:01:07 +0800

    fix setup config script

--- setup-config.sh
+++ setup-config.sh
@@ -3,14 +3,16 @@
 if [ -z "USE_ALPINE" ]
 then
   docker run -i --rm kiritaniayaka/pg_jieba \
-    cat /usr/share/postgresql/postgresql.conf.sample > ./config/postgresql.conf
+    cat /usr/share/postgresql/postgresql.conf.sample > ./postgresql.conf
 else
  docker run -i --rm kiritaniayaka/pg_jieba:alpine \
-    cat /usr/local/share/postgresql/postgresql.conf.sample > ./config/postgresql.conf  
+    cat /usr/local/share/postgresql/postgresql.conf.sample > ./postgresql.conf  
 fi
 
 sed -i "/^#shared_preload_libraries/ {
   n
   i\
 shared_preload_libraries = 'pg_jieba.so'
-}" ./postgresql/postgresql.conf
+}" ./postgresql.conf
+
+echo 'Saved at ./postgresql.conf'
